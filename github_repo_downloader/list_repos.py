f61a4e63eb702bb212ec0b18fdfe241c006bd18d
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2022-12-14 16:05:43 -0300

    Wrap reusable logic in a function (#3)

--- github_repo_downloader/list_repos.py
+++ github_repo_downloader/list_repos.py
@@ -2,21 +2,18 @@ import dotenv
 import os
 from typing import Literal
 
-from tap import Tap
 from github import Github
+from tap import Tap
+
+dotenv.load_dotenv()
+
 
 class Args(Tap):
     org: str  # Github Organization
     output: Literal["http", "ssh", "default"] = "default"  # Choose URL protocol.
-#functional progarming it so i dosent use global variables:
-args = Args(underscores_to_dashes=True).parse_args()
 
 
-dotenv.load_dotenv()
-g = Github(os.getenv("GITHUB_ACESS_TOKEN"))
-
-
-def lis_repos(github, org):
+def list_repos(github, org):
     for repo in github.get_organization(org).get_repos():
         print(f"{args.org}/{repo.name}")
 
@@ -29,10 +26,17 @@ def list_repos_ssh(github, org):
      for repo in github.get_organization(org).get_repos():
         print(f"git@github.com:{args.org}/{repo.name}")
 
-if __name__ == "__main__":
+
+def main(github_client: Github, org_name: str):
     if args.output == "ssh":
-        list_repos_ssh(g, args.org)
+        list_repos_ssh(github_client, org_name)
     elif args.output == "http":
-        list_repos_http(g, args.org)
+        list_repos_http(github_client, org_name)
     elif args.output == "default":
-        lis_repos(g, args.org)
+        list_repos(github_client, org_name)
+
+
+if __name__ == "__main__":
+    g = Github(os.getenv("GITHUB_ACESS_TOKEN"))
+    args = Args(underscores_to_dashes=True).parse_args()
+    main(g, args.org)
