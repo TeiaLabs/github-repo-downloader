f08125d0fa0353e31005b8c3fd71d334330f6193
RamiroNB <ramiro.nilson.barros@gmail.com>
2022-12-14 15:22:44 -0300

    Remove global vaiabals from functions in list_repos

--- github_repo_downloader/list_repos.py
+++ github_repo_downloader/list_repos.py
@@ -8,7 +8,7 @@ from github import Github
 class Args(Tap):
     org: str  # Github Organization
     output: Literal["http", "ssh", "default"] = "default"  # Choose URL protocol.
-
+#functional progarming it so i dosent use global variables:
 args = Args(underscores_to_dashes=True).parse_args()
 
 
@@ -16,23 +16,23 @@ dotenv.load_dotenv()
 g = Github(os.getenv("GITHUB_ACESS_TOKEN"))
 
 
-def lis_repos():
-    for repo in g.get_organization(args.org).get_repos():
+def lis_repos(github, org):
+    for repo in github.get_organization(org).get_repos():
         print(f"{args.org}/{repo.name}")
 
-def list_repos_http():
-    for repo in g.get_organization(args.org).get_repos():
-        print(f"https://github.com/{args.org}/{repo.name}")
+def list_repos_http(github, org):
+    for repo in github.get_organization(org).get_repos():
+        print(f"https://github.com/{org}/{repo.name}")
 
 
-def list_repos_ssh():
-     for repo in g.get_organization(args.org).get_repos():
+def list_repos_ssh(github, org):
+     for repo in github.get_organization(org).get_repos():
         print(f"git@github.com:{args.org}/{repo.name}")
 
 if __name__ == "__main__":
     if args.output == "ssh":
-        list_repos_ssh()
+        list_repos_ssh(g, args.org)
     elif args.output == "http":
-        list_repos_http()
+        list_repos_http(g, args.org)
     elif args.output == "default":
-        lis_repos()
+        lis_repos(g, args.org)
